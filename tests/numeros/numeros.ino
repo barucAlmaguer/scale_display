163bb4db8832dd00c6b20e3251e3b660cc3e854c
baruc <baruc.almaguer@gmail.com>
2017-09-14 18:49:28 -0500

    initial commit (test display 7 segs)

---
+++ tests/numeros/numeros.ino
@@ -0,0 +1,31 @@
+//  Author:Frankie.Chu
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#include "TM1637.h"
+#define CLK 2//pins definitions for TM1637 and can be changed to other ports       
+#define DIO 3
+TM1637 tm1637(CLK,DIO);
+
+void setup()
+{
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+}
+void loop()
+{
+  for (int i = 0; i < 32; i++){
+    tm1637.display(i%4,i);
+    delay(500);
+  }
+}
